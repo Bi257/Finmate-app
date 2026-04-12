0e7dfbb5d166cf6a5dc5a559cf3cdc7629e00bd0
Bi257 <dluu5032@gamil.com>
2026-04-13 02:11:24 +0700

    feat: update core logic, database, exchange rate

--- app/src/main/java/com/example/baitapdidongcuoiki/data/mapper/TransactionMapper.kt
+++ app/src/main/java/com/example/baitapdidongcuoiki/data/mapper/TransactionMapper.kt
@@ -3,10 +3,10 @@ package com.example.baitapdidongcuoiki.data.mapper
 import com.example.baitapdidongcuoiki.data.local.entity.TransactionEntity
 import com.example.baitapdidongcuoiki.domain.model.Transaction
 
-
-fun Transaction.toEntity(): TransactionEntity {
+fun Transaction.toEntity(userId: String): TransactionEntity {
     return TransactionEntity(
         id = id ?: 0,
+        userId = userId,
         title = title,
         amount = amount,
         type = type,
@@ -14,6 +14,7 @@ fun Transaction.toEntity(): TransactionEntity {
         note = note
     )
 }
+
 fun TransactionEntity.toDomain(): Transaction {
     return Transaction(
         id = id,
