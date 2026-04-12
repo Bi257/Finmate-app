9fe6417856820d87968f5e200108e1bc5bf35c51
Bi257 <142707269+Bi257@users.noreply.github.com>
2026-04-13 02:21:41 +0700

    Revert "Revert "feat: update core logic, database, exchange rate""

--- app/src/main/java/com/example/baitapdidongcuoiki/data/local/TransactionDao.kt
+++ app/src/main/java/com/example/baitapdidongcuoiki/data/local/TransactionDao.kt
@@ -7,12 +7,12 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface TransactionDao {
 
-    @Query("SELECT * FROM transactions ORDER BY date DESC")
-    fun getAll(): Flow<List<TransactionEntity>>
+    @Query("SELECT * FROM transactions WHERE userId = :userId ORDER BY date DESC")
+    fun getTransactionsByUserId(userId: String): Flow<List<TransactionEntity>>
 
     @Insert
     suspend fun insert(transaction: TransactionEntity)
 
-    @Query("SELECT * FROM transactions")
-    suspend fun getAllOnce(): List<TransactionEntity>
+    @Query("SELECT * FROM transactions WHERE userId = :userId")
+    suspend fun getAllOnceByUserId(userId: String): List<TransactionEntity>
 }
