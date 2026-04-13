8d1470fc5a99f29c07e5352eb55d47249def8c4c
Bi257 <142707269+Bi257@users.noreply.github.com>
2026-04-13 23:09:48 +0700

    Revert "feat: readme1"

--- app/src/main/java/com/example/baitapdidongcuoiki/data/local/AppDatabase.kt
+++ app/src/main/java/com/example/baitapdidongcuoiki/data/local/AppDatabase.kt
@@ -6,7 +6,7 @@ import com.example.baitapdidongcuoiki.data.local.entity.TransactionEntity
 
 @Database(
     entities = [TransactionEntity::class, ExchangeRateEntity::class],
-    version = 3,
+    version = 3, // 🔥 tăng version
     exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
